df7c98c7bc3822293a8300a20164da32236dcd5f
luojiyin <luojiyin@hotmail.com>
2023-04-03 23:01:16 +0800

    Complete the prototype

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,24 +26,25 @@ import { optionValue, licenseTips } from "../components/helper";
 const Home: NextPage = () => {
   const [stepIndex, setStepIndex] = useState(0);
   const [filterOption, setFilterOption] = useState({});
-  const list = filterLicenses({
-    popularity: FeatureAttitude.Negative,
-    reuseCondition: FeatureAttitude.Positive,
-    infectionIntensity: FeatureAttitude.Positive,
-    infectionRange: InfectionRange.Module,
-    jurisdiction: FeatureAttitude.Undefined,
-    patentStatement: FeatureAttitude.Positive,
-    patentRetaliation: FeatureAttitude.Positive,
-    enhancedAttribution: FeatureAttitude.Positive,
-    privacyLoophole: FeatureAttitude.Negative,
-    marketingEndorsement: FeatureAttitude.Negative,
-  });
+  const [lists, setLists] = useState([]);
+  // const list = filterLicenses({
+  //   popularity: FeatureAttitude.Negative,
+  //   reuseCondition: FeatureAttitude.Positive,
+  //   infectionIntensity: FeatureAttitude.Positive,
+  //   infectionRange: InfectionRange.Module,
+  //   jurisdiction: FeatureAttitude.Undefined,
+  //   patentStatement: FeatureAttitude.Positive,
+  //   patentRetaliation: FeatureAttitude.Positive,
+  //   enhancedAttribution: FeatureAttitude.Positive,
+  //   privacyLoophole: FeatureAttitude.Negative,
+  //   marketingEndorsement: FeatureAttitude.Negative,
+  // });
 
   const chooseSteps: string[] = [
     "popularity",
     "reuseCondition",
     "infectionIntensity",
-    "jurisdiction",
+    "infectionRange",
     "jurisdiction",
     "patentStatement",
     "patentRetaliation",
@@ -53,7 +54,7 @@ const Home: NextPage = () => {
   ];
 
   // rome-ignore lint/style/useConst: <explanation>
-let  choose: string | null = null;
+  let choose: string | null = null;
 
   const now = Math.ceil(100 / chooseSteps.length);
   //chooseSteps.map((step)=> console.log(optionValue[step]))
@@ -61,16 +62,22 @@ let  choose: string | null = null;
   // console.log("list");
   // console.log(list); // filtered licenses
 
-  const handleSelect = (e:string) =>{
-   
-    console.log("handleSelect choice,e",stepIndex,  e);
+  const handleSelect = (e: string) => {
+    console.log("handleSelect choice,e", stepIndex, e);
+    const choice = ["0", "1", "-1"].includes(e) ? Number(e) : e;
     console.log(chooseSteps[stepIndex]);
-    const  key =  chooseSteps[stepIndex];
-    const newObject = {...filterOption, [key]: e}
-    setFilterOption({...newObject });
-    console.log(filterOption)
-    setStepIndex(stepIndex<chooseSteps.length-1? stepIndex+1:0)
-  }
+    const key = chooseSteps[stepIndex];
+    const newObject = { ...filterOption, [key]: choice };
+
+    console.log(filterOption);
+
+    const tempLists = filterLicenses(newObject);
+    setStepIndex(stepIndex < chooseSteps.length - 1 ? stepIndex + 1 : stepIndex);
+    setFilterOption({ ...newObject });
+    setLists(tempLists);
+    console.log("tempLists", tempLists);
+    console.log("list", lists);
+  };
 
   return (
     <div className={styles.container}>
@@ -85,7 +92,7 @@ let  choose: string | null = null;
         该工具旨在帮助用户理解他们自己对于自由和开源软件许可协议的偏好。用户必须自己阅读这些许可协议。在将许可协议适用于您的财产之前，阅读并完全理解您选择的许可协议是非常重要的。支撑该工具运行的许可类型分类，会不可避免地有些缩减。因此，不能也切不可将该工具的输出信息视为法律意见。
       </p>
       <h4 className="warning black">切记：必须阅读并理解您选择的许可协议。</h4>
-      
+
       <div>
         {licenseTips[chooseSteps[stepIndex]].map((tip) => (
           <p>{tip.text}</p>
@@ -111,12 +118,18 @@ let  choose: string | null = null;
           </Dropdown.Toggle>
           <Dropdown.Menu>
             {optionValue[chooseSteps[stepIndex]].map((info) => (
-              <Dropdown.Item  eventKey={ info.value}>{info.text}</Dropdown.Item>
+              <Dropdown.Item eventKey={info.value}>{info.text}</Dropdown.Item>
             ))}
           </Dropdown.Menu>
         </Dropdown>
       </div>
-
+      <div>
+        {lists.map((list, index) => (
+          <p>
+            {list.license.name} score: {list.score * 10}
+          </p>
+        ))}
+      </div>
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
